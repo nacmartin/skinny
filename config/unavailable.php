52ad72c30d6bd43d91fe040bd2a390cbc14f80fc
Nacho Martín <nitram.ohcan@gmail.com>
2010-03-07 01:41:06 +0100

    scheduled manteinance

--- config/unavailable.php
+++ config/unavailable.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>Oops! an error ocurred</title>
+        <title>Temporarly unavailable</title>
     <link rel="shortcut icon" href="/favicon.ico" />
     <link rel="stylesheet" type="text/css" media="screen" href="/css/main.css" />
   </head>
@@ -15,11 +15,10 @@
 
     <div class="wrapAll">
       <a href="/"><img src="/images/listandcheck.png" /></a>      <center>
-        <h1>An error ocurred in our machines.</h1>
-<p>Please e-mail us at contact@listandcheck.com 
+        <h1>We are doing scheduled manteinance.</h1>
+<p>Please retry in some minutes 
 Sorry for any inconvenience caused.</p>
 <p><a href="#" onClick="history.back(); return false;">Back to previous page</a></p>
-<p><a href="/">List&check home page</a></p>
 
 </center>
     </div>
